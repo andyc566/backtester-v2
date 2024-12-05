43a5c8a2db61488e1e7b6d6762a644941b6a4ac0
Andy Chen <andymchen566@gmail.com>
2024-12-04 22:17:16 -0700

    updated utils

--- utils/test1.py
+++ utils/test1.py
@@ -3,3 +3,46 @@ def main():
 
 if __name__ == "__main__": 
     main() 
+
+
+'''
+
+Sub RunPythonScript()
+    Dim pythonExe As String
+    Dim scriptPath As String
+    Dim shellOutput As String
+    Dim cmd As String
+    Dim wsh As Object
+    Dim process As Object
+    Dim stdout As Object
+    
+    ' Specify the path to the Python executable
+    pythonExe = "C:\Program Files\Python312\python.exe"
+    
+    ' Specify the path to the Python script
+    scriptPath = "C:\Users\chena\Desktop\code\backtesterv2\utils\test1.py"
+    
+    ' Construct the command to run the Python script
+    cmd = pythonExe & " """ & scriptPath & """"
+    
+    ' Use Windows Script Host to execute the command and capture the output
+    On Error GoTo ErrorHandler
+    Set wsh = CreateObject("WScript.Shell")
+    Set process = wsh.Exec(cmd)
+    Set stdout = process.stdout
+    
+    ' Capture the Python script's output
+    shellOutput = stdout.ReadAll
+    
+    ' Display the result in a message box
+    MsgBox shellOutput, vbInformation, "Python Script Output"
+    
+    Exit Sub
+
+ErrorHandler:
+    MsgBox "An error occurred while running the Python script: " & Err.Description, vbCritical, "Error"
+End Sub
+
+
+
+'''
--- utils/test2.py
+++ utils/test2.py
@@ -6,3 +6,40 @@ def main():
 
 if __name__ == "__main__": 
     main() 
+
+'''
+Sub TestImageInsert()
+    Dim imagePath As String
+    Dim ws As Worksheet
+    Dim imgLeft As Double
+    Dim imgTop As Double
+    
+    imagePath = "C:\Users\chena\Desktop\code\iso-ne-energy-forecast\data\plots\predictions_plot.png"
+    
+    ' Confirm the file exists
+    If Dir(imagePath) = "" Then
+        MsgBox "File not found: " & imagePath, vbCritical, "Error"
+        Exit Sub
+    End If
+    
+    ' Insert the image
+    Set ws = ThisWorkbook.ActiveSheet
+    imgLeft = ws.Range("A1").Left
+    imgTop = ws.Range("A1").Top
+    
+    On Error Resume Next
+    ws.Pictures.Insert(imagePath).Select
+    If Err.Number <> 0 Then
+        MsgBox "Error inserting image: " & Err.Description, vbCritical, "Error"
+    Else
+        Selection.ShapeRange.LockAspectRatio = msoTrue ' Maintain aspect ratio
+        Selection.ShapeRange.Left = imgLeft
+        Selection.ShapeRange.Top = imgTop
+        MsgBox "Image inserted successfully!", vbInformation, "Success"
+    End If
+    On Error GoTo 0
+End Sub
+
+
+
+'''
